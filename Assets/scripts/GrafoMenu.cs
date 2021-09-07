43a477b3383ab12083599a1e44d84112b85b64fb
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2021-09-07 16:07:02 -0300

    Melhoramentos do Hud do Menu

--- Assets/scripts/GrafoMenu.cs
+++ Assets/scripts/GrafoMenu.cs
@@ -3,29 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class force : MonoBehaviour {
+public class GrafoMenu : MonoBehaviour {
 
 	public List<Nodes> nodes;
-	public Material matlines,matpoints;
+	public Material matlines, matpoints;
 	public GameObject nodeobject;
 	public int[,] matrix = new int[3,2];
 	public int[] vec;
 	public Transform target;
+	public int grafoTam;
 	// Use this for initialization
 	void Start () {
-		nodes =  new List<Nodes>();
-		for(int i = 0; i < 10; i ++){
-			nodes.Add(new Nodes() {childrens = nodes.Where(node => Random.value > 0.5f).ToList(),pos = Random.insideUnitSphere * 8,vel = Vector3.zero});
-		}
-		foreach(var node in nodes){
-			Points(node.pos);
-		}
+		CriarGrafo ();
+
 	}
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(target.position);
 		transform.Translate(Vector3.right * 1 * Time.deltaTime);
 
+	}
+	void CriarGrafo(){
+		nodes =  new List<Nodes>();
+		for(int i = 0; i < grafoTam; i ++){
+			nodes.Add(new Nodes() {childrens = nodes.Where(node => Random.value > 0.5f).ToList(),pos = Random.insideUnitSphere * 8,vel = Vector3.zero});
+		}
+		foreach(var node in nodes){
+			Points(node.pos);
+		}
 	}
 	void OnPostRender(){
 		Lines();
