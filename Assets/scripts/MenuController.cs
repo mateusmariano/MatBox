6c303176cb4fcadfe7dd94d44d4cbdb40a635cc1
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2021-10-16 21:53:06 -0300

    Algumas correções para os defeitos do LogTesteMobile

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+	 
+	public void matriz (){
+		SceneManager.LoadScene ("MatrizAdjacencia");
+	}
+	public void coord (){
+		Application.LoadLevel("CoordHomo");
+		
+	}
+	public void pilha (){
+		Application.LoadLevel("Pilha");
+		
+	}
+	public void fila (){
+		Application.LoadLevel("Fila");
+
+	}
+}
